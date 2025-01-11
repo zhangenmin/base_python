4efc2fccf68b42b0f7aa341ce7cc95f4f99102bb
张恩民 <306206438@qq.com>
2025-01-11 23:52:09 +0800

    pop的练习

--- list.py
+++ list.py
@@ -7,7 +7,24 @@ print(squares)
 number_20 = [value for value in range(1,21)]
 print(number_20)
 
-number_mililion = [value for value in range(1,100000001)]
-print(min(number_mililion))
-print(max(number_mililion))
-print(sum(number_mililion))
+# number_mililion = [value for value in range(1,100000001)]
+# print(min(number_mililion))
+# print(max(number_mililion))
+# print(sum(number_mililion))
+
+
+players = ['charles','martina','michael','florence','eli']
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
+
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+
+
+motorcycles = ['honda','yamaha','suzuki']
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a "+last_owned.title()+".")
