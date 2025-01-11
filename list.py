78de107c87ecf1a9c5345b7d49103a5cce5e8458
张恩民 <306206438@qq.com>
2025-01-11 23:31:55 +0800

    加了一些练习

--- list.py
+++ list.py
@@ -4,3 +4,10 @@
 # @File    : list.py
 squares = [value**2 for value in range(1,11)]
 print(squares)
+number_20 = [value for value in range(1,21)]
+print(number_20)
+
+number_mililion = [value for value in range(1,100000001)]
+print(min(number_mililion))
+print(max(number_mililion))
+print(sum(number_mililion))
